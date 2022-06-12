9b6c132cef6e1afa260ebc4cd8272c6edbbe4d5b
chunuhhc <so8151@gmail.com>
2022-06-12 12:17:52 +0800

    update

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ $(document).ready(function () {
       },
       mousewheel: true,
       keyboard: true,
+      autoplay: {
+        delay: 3500,
+      },
   
       observer: true, //修改swiper自己或子元素时，自动初始化swiper
       observeParents: true, //修改swiper的父元素时，自动初始化swiper

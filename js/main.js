7c473c0c804811c118845a7547cd91ee8d2f5e2b
chunuhhc <so8151@gmail.com>
2022-08-17 15:01:10 +0800

    0817

--- js/main.js
+++ js/main.js
@@ -1,35 +1,137 @@
 
-window.onload = function() {
+// window.onload = function() {
 
+//   var header = document.getElementById('header');
+//   var footer = document.getElementById('footer');
+
+//   axios.get('./header.html')
+//   .then(function(res) {
+//       // load('./header.html')
+//       header.innerHTML = res.data;
+
+//       var now = document.getElementsByClassName('navBar');
+//       // console.log(now[0].children);
+//       var url = window.location.href;
+
+
+
+//       // var urlLoca = url.slice(url.indexOf("index"))
+//       // console.log(url.indexOf("/",1));
+
+//       // console.log(url.indexOf("index"));
+//       // var str = url.slice(22, 23);
+
+//       // console.log(str);
+      
+//       // switch
+      
+//       // 篩選標籤顯示圖
+//       // 切換瀑布流顯示
+      
+      
+//       // if(now === urlLoca) {
+//       //   console.log(yes);
+//       // }
+      
+
+//   })
+
+//   // axios.get('./footer.html')
+//   // .then(function(res) {
+//   //   footer.innerHTML = res.data;
+//   // })
+
+
+
+
+//   // // 滾動釘選header修改中
+//   // var body = document.getElementsByTagName('body');
+//   // var headerH = header.offsetHeight();
+  
+//   // // body.addEventListener('scroll', headerH) {
+    
+//   // // }
+
+//   // if (body.scrollTop > headerH) {
+//   //   document.getElementById('origiHeader').hide();
+//   //   document.getElementById('fixedHeader').show();
+//   // }
+
+
+
+
+//   // .catch(function(err) {
+//   //     console.log(err);
+//   // });
+
+
+//   // $('ul.navBar a').click(function() {
+//   //   console.log(this);
+    
+//   //   this.addClass('active').siblings.removeClass('active');
+//   // })
+
+
+
+
+  
+//           //   // 點擊切換分頁參考
+//           //   $("#parentHTab").each(function () {
+//           //     var $li = $("ul.tab li");
+//           //     $($li.eq(0).addClass("active").find("a").attr("href"))
+//           //         .siblings(".tab-content")
+//           //         .hide();
+
+//           //     $li.click(function () {
+//           //         $($(this).find("a").attr("href"))
+//           //             .show()
+//           //             .siblings(".tab-content")
+//           //             .hide();
+
+//           //         $(this)
+//           //             .addClass("active")
+//           //             .siblings(".active")
+//           //             .removeClass("active");
+
+//           //         if ($(this).hasClass("myCalendar")) {
+//           //             calendar.render();
+//           //         };
+//           //         return false;
+//           //     });
+//           // });
+// }
+
+$(document).ready(function () {
+  console.log(234);
   var header = document.getElementById('header');
   var footer = document.getElementById('footer');
 
   axios.get('./header.html')
   .then(function(res) {
+
       // load('./header.html')
       header.innerHTML = res.data;
 
-      var now = document.getElementsByClassName('navBar');
-      console.log(now[0].children);
-      var url = window.location.href;
-      // var urlLoca = url.slice(url.indexOf("index"))
-      // console.log(url.indexOf("/",1));
+      // 點擊切換分頁
+      $('.tabLink').click(function() {
+        // var $li = $('ul.navBar li a');
+        // console.log(123);
+        // console.log($(this).attr('data-href'));
+        var url = $(this).attr('href');
+        
+        $(this).addClass('active')
+        .parent().siblings().find('a').removeClass('active');
+        $(url).fadeIn(1000)
+        .siblings('.tabContent').fadeOut(1000);
+        return false;
+      });
 
-      // console.log(url.indexOf("index"));
-      // var str = url.slice(22, 23);
-
-      // console.log(str);
-      
-      // switch
-      
-      // 篩選標籤顯示圖
-      // 切換瀑布流顯示
-      
-      
-      // if(now === urlLoca) {
-      //   console.log(yes);
-      // }
-      
+      // 預設值
+      $('#aboutTab').addClass('active')
+      .parent().siblings().find('a').removeClass('active');
+      $('#aboutTabContent').fadeIn(1000)
+        .siblings('.tabContent').fadeOut(1000);
+        return false;
   })
 
   axios.get('./footer.html')
@@ -38,79 +140,28 @@ window.onload = function() {
   })
 
 
-
-
-  // 滾動釘選header修改中
-  var body = document.getElementsByTagName('body');
-  var headerH = header.offsetHeight();
+  // // swiper
+  // var swiper = new Swiper(".swiper-container", {
+  //     cssMode: true,
+  //     navigation: {
+  //         nextEl: ".swiper-button-next",
+  //         prevEl: ".swiper-button-prev",
+  //     },
+  //     pagination: {
+  //         el: ".swiper-pagination",
+  //     },
+  //     mousewheel: true,
+  //     keyboard: true,
+  //     autoplay: {
+  //       delay: 3500,
+  //     },
   
-  // body.addEventListener('scroll', headerH) {
-    
-  // }
-
-  if (body.scrollTop > headerH) {
-    document.getElementById('origiHeader').hide();
-    document.getElementById('fixedHeader').show();
-  }
-
-  // .catch(function(err) {
-  //     console.log(err);
+  //     observer: true, //修改swiper自己或子元素时，自动初始化swiper
+  //     observeParents: true, //修改swiper的父元素时，自动初始化swiper
   // });
 
 
-  // $('ul.navBar a').click(function() {
-  //   console.log(this);
-    
-  //   this.addClass('active').siblings.removeClass('active');
-  // })
-
-
   
-            // 點擊切換分頁參考
-            $("#parentHTab").each(function () {
-              var $li = $("ul.tab li");
-              $($li.eq(0).addClass("active").find("a").attr("href"))
-                  .siblings(".tab-content")
-                  .hide();
-
-              $li.click(function () {
-                  $($(this).find("a").attr("href"))
-                      .show()
-                      .siblings(".tab-content")
-                      .hide();
-
-                  $(this)
-                      .addClass("active")
-                      .siblings(".active")
-                      .removeClass("active");
-
-                  if ($(this).hasClass("myCalendar")) {
-                      calendar.render();
-                  };
-                  return false;
-              });
-          });
-}
-
-$(document).ready(function () {
-
-  var swiper = new Swiper(".swiper-container", {
-      cssMode: true,
-      navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-          el: ".swiper-pagination",
-      },
-      mousewheel: true,
-      keyboard: true,
-      autoplay: {
-        delay: 3500,
-      },
-  
-      observer: true, //修改swiper自己或子元素时，自动初始化swiper
-      observeParents: true, //修改swiper的父元素时，自动初始化swiper
-  });
+ 
 
 })

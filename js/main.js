a538a9626edd85abb69e66cb8e1d903d1e3c7f5c
chunuhhc <so8151@gmail.com>
2022-08-21 13:05:13 +0800

    0821

--- js/main.js
+++ js/main.js
@@ -271,35 +271,6 @@ $(document).ready(function () {
 
   }
 
-  // var sBtnItem = document.getElementsByClassName('sBtnItem')
-
-  // sBtnItem.addEventListener('click', function() {
-  //   if(this.id == 'allBtn') {
-  //     console.log('yes');
-  //     allBlock();
-  //   } else {
-  //     console.log('no');
-  //     for(var s = 0; s < sTag.length; s++) {
-  //       sTag[s].addEventListener('click', tagSearch);
-  //     }
-  //   }
-  // })
-
-  // $('#sBtn button').click(function() {
-  //   if(this.id == 'allBtn') {
-  //     console.log('yes');
-  //     allBlock();
-  //   } else {
-  //     console.log('no');
-  //     for(var s = 0; s < sTag.length; s++) {
-  //       sTag[s].addEventListener('click', tagSearch);
-  //     }
-  //   }
-  // })
-
-  // sArr = pContent;
-  // sArr.push(pContent);
-
 
   // 全部顯示
   function allBlock() {
@@ -308,17 +279,13 @@ $(document).ready(function () {
     sContent.html(html);
 
     sArr =  Object.entries(pContent);
-    // console.log('allBlock : '+sArr);
-    
-    // console.log(sArr[0][1][0].classN);
     
   
     var str = '';
     for(var i = 0; i < sArr.length; i++) {
-      // console.log(sArr[i]);
       for(var j = 0; j < sArr[i][1].length; j++) {
         str = sArr[i][1][j];
-        html += '<a class="'+str.classN+'" href="'+str.href+'" target="'+str.target+'">'
+        html += '<a class="'+str.classN+'" href="'+str.href+'" target="'+str.target+'" data-aos="fade-right">'
         html += '<div class="hover"><div class="content">'
         html += '<div class="hashtag">'+str.hashtag+'</div>'
         html += '<div class="title">'+str.title+'</div></div></div>'
@@ -328,17 +295,12 @@ $(document).ready(function () {
     }
     sContent.html(html);
     loadMore(sArr.length);
+
+    $(".inline").colorbox({inline: true, width: "50%"});
     
   }
 
   allBlock();
-  // loadMore(sArr.length);
-
-
-
-  // console.log('1 '+sContent);
-  // sContent.html(html);
-  // console.log(sContent);
 
   
 
@@ -346,25 +308,11 @@ $(document).ready(function () {
 
     html = '';
     sContent.html(html);
-
-    // console.log(this.innerText);
-    // console.log(pContent[this.innerText]);
-    
-    // search.value = this.innerText;
     sArr = pContent[sText];
-
-
-    // searchBar.val(sText);
-    // console.log($('#searchBar:text'));
-    
-    // console.log('sArr '+pContent[sText]);
     
     
     for(var i = 0; i < sArr.length; i++) {
-      // console.log(sArr[i]);
-      
-      // html += '<a class="'+sArr[i].classN+'" href="'+sArr[i].href+'">'
-      html += '<a class="'+sArr[i].classN+'" href="'+sArr[i].href+'" target="'+sArr[i].target+'">'
+      html += '<a class="'+sArr[i].classN+'" href="'+sArr[i].href+'" target="'+sArr[i].target+'" data-aos="fade-right">'
       html += '<div class="hover"><div class="content">'
       html += '<div class="hashtag">'+sArr[i].hashtag+'</div>'
       html += '<div class="title">'+sArr[i].title+'</div></div></div>'
@@ -375,6 +323,8 @@ $(document).ready(function () {
       // console.log(sContent);
 
       loadMore(sArr.length);
+
+      $(".inline").colorbox({inline: true, width: "50%"});
       
   }
   // for迴圈不能使用匿名函示
@@ -486,37 +436,39 @@ $(document).ready(function () {
 
   ////// --colorbox begin--
   //Examples of how to assign the Colorbox event to elements
-  $(".group1").colorbox({rel:'group1'});
-  $(".group2").colorbox({rel:'group2', transition:"fade"});
-  $(".group3").colorbox({rel:'group3', transition:"none", width:"75%", height:"75%"});
-  $(".group4").colorbox({rel:'group4', slideshow:true});
-  $(".ajax").colorbox();
-  $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
-  $(".vimeo").colorbox({iframe:true, innerWidth:500, innerHeight:409});
-  $(".iframe").colorbox({iframe:true, width:"80%", height:"80%"});
+  // $(".group1").colorbox({rel:'group1'});
+  // $(".group2").colorbox({rel:'group2', transition:"fade"});
+  // $(".group3").colorbox({rel:'group3', transition:"none", width:"75%", height:"75%"});
+  // $(".group4").colorbox({rel:'group4', slideshow:true});
+  // $(".ajax").colorbox();
+  // $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+  // $(".vimeo").colorbox({iframe:true, innerWidth:500, innerHeight:409});
+  // $(".iframe").colorbox({iframe:true, width:"80%", height:"80%"});
   $(".inline").colorbox({inline:true, width:"50%"});
-  $(".callbacks").colorbox({
-      onOpen:function(){ alert('onOpen: colorbox is about to open'); },
-      onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
-      onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
-      onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
-      onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
-  });
+  // $(".callbacks").colorbox({
+  //     onOpen:function(){ alert('onOpen: colorbox is about to open'); },
+  //     onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
+  //     onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
+  //     onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
+  //     onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
+  // });
 
-  $('.non-retina').colorbox({rel:'group5', transition:'none'})
-  $('.retina').colorbox({rel:'group5', transition:'none', retinaImage:true, retinaUrl:true});
+  // $('.non-retina').colorbox({rel:'group5', transition:'none'})
+  // $('.retina').colorbox({rel:'group5', transition:'none', retinaImage:true, retinaUrl:true});
   
-  //Example of preserving a JavaScript event for inline calls.
-  $("#click").click(function(){ 
-      $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
-      return false;
-  });
+  // //Example of preserving a JavaScript event for inline calls.
+  // $("#click").click(function(){ 
+  //     $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
+  //     return false;
+  // });
 
   ////// --colorbox end--
 
-  ////// --lazyload begin--
-  // $('img').lazyload();
-  ////// --lazyload end--
+  ////// --AOS begin--
+
+  AOS.init();
+
+  ////// --AOS end--
 
 
 })

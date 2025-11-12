3fac27fda45537b0f64c02eab8ea3cacdc1973ab
so8151 <so8151@gmail.com>
2025-11-12 14:16:45 +0800

    暫時關閉acgworld

--- js/main.js
+++ js/main.js
@@ -1,323 +1,316 @@
 $(document).ready(function () {
-
   ////// HEADER&FOOTER LOAD begin
 
-  var headerDom = document.getElementById('header');
-  var footerDom = document.getElementById('footer');
+  var headerDom = document.getElementById("header");
+  var footerDom = document.getElementById("footer");
 
   function headerData() {
-    return axios.get('./header.html');
+    return axios.get("./header.html");
   }
 
   function footerData() {
-    return axios.get('./footer.html');
+    return axios.get("./footer.html");
   }
 
-  axios.all([headerData(), footerData()])
-  .then(axios.spread(function (header, footer) {
-    
-    headerDom.innerHTML = header.data;
-    footerDom.innerHTML = footer.data;
-    
-    // 點擊切換分頁
-    $('.tabLink').click(function() {
-      var url = $(this).attr('href');
-      
-      $(this).addClass('active')
-      .parent().siblings().find('a').removeClass('active');
-      $(url).siblings('.tabContent').fadeOut(500);
-      $(url).delay(500).fadeTo(500, 1);
-      return false;
-    });
+  axios.all([headerData(), footerData()]).then(
+    axios.spread(function (header, footer) {
+      headerDom.innerHTML = header.data;
+      footerDom.innerHTML = footer.data;
 
-    // 預設值
-    function resetFun() {
-      $('#allTab').addClass('active')
-      .parent().siblings().find('a').removeClass('active');
-      $('#allTabContent').fadeIn(500)
-        .siblings('.tabContent').fadeOut(500);
+      // 點擊切換分頁
+      $(".tabLink").click(function () {
+        var url = $(this).attr("href");
+
+        $(this)
+          .addClass("active")
+          .parent()
+          .siblings()
+          .find("a")
+          .removeClass("active");
+        $(url).siblings(".tabContent").fadeOut(500);
+        $(url).delay(500).fadeTo(500, 1);
         return false;
-        console.log('reset');
-        
-    };
+      });
 
-  }));
+      // 預設值
+      function resetFun() {
+        $("#allTab")
+          .addClass("active")
+          .parent()
+          .siblings()
+          .find("a")
+          .removeClass("active");
+        $("#allTabContent").fadeIn(500).siblings(".tabContent").fadeOut(500);
+        return false;
+        console.log("reset");
+      }
+    })
+  );
 
   ////// HEADER&FOOTER LOAD end
-
-
-
-
 
   ////// SEARCH begin--
 
   var pContent = {
-    "UI設計切版": [
+    UI設計切版: [
       {
-        hashtag: ['UI設計', '切版'],
-        title: '線上櫃台功能設計切版',
-        href: '#counter01',
-        classN: 'inline',
-        target: '',
-        src: 'img/counter01-th.webp'
+        hashtag: ["UI設計", "切版"],
+        title: "線上櫃台功能設計切版",
+        href: "#counter01",
+        classN: "inline",
+        target: "",
+        src: "img/counter01-th.webp",
+      },
+      // {
+      //   hashtag: ['UI設計', '切版'],
+      //   title: '電商活動頁',
+      //   href: 'https://www.jollybuy.com/act/acgworld/',
+      //   classN: '',
+      //   target: '_blank',
+      //   src: 'img/jb01.jpg'
+      // },
+      {
+        hashtag: ["UI設計", "切版"],
+        title: "高中段考詳細頁設計切版",
+        href: "https://www.tkbgo.com.tw/go_edm/edm665/index.jsp",
+        classN: "",
+        target: "_blank",
+        src: "img/edm02-th.webp",
       },
       {
-        hashtag: ['UI設計', '切版'],
-        title: '電商活動頁',
-        href: 'https://www.jollybuy.com/act/acgworld/',
-        classN: '',
-        target: '_blank',
-        src: 'img/jb01.jpg'
+        hashtag: ["UI設計", "切版"],
+        title: "外交官特考詳細頁設計切版",
+        href: "https://www.tkbgo.com.tw/go_edm/edm215/index.jsp",
+        classN: "",
+        target: "_blank",
+        src: "img/edm03-th.webp",
       },
       {
-        hashtag: ['UI設計', '切版'],
-        title: '高中段考詳細頁設計切版',
-        href: 'https://www.tkbgo.com.tw/go_edm/edm665/index.jsp',
-        classN: '',
-        target: '_blank',
-        src: 'img/edm02-th.webp'
+        hashtag: ["UI設計", "切版"],
+        title: "上榜頂大詳細頁設計切版",
+        href: "https://www.tkbgo.com.tw/go_edm/edm220/index.jsp",
+        classN: "",
+        target: "_blank",
+        src: "img/edm01-th.webp",
       },
       {
-        hashtag: ['UI設計', '切版'],
-        title: '外交官特考詳細頁設計切版',
-        href: 'https://www.tkbgo.com.tw/go_edm/edm215/index.jsp',
-        classN: '',
-        target: '_blank',
-        src: 'img/edm03-th.webp'
+        hashtag: ["UI設計", "切版"],
+        title: "中鋼行銷頁設計切版",
+        href: "https://www.tkbgo.com.tw/go_edm/edm178/index.jsp",
+        classN: "",
+        target: "_blank",
+        src: "img/edm05-th.webp",
       },
       {
-        hashtag: ['UI設計', '切版'],
-        title: '上榜頂大詳細頁設計切版',
-        href: 'https://www.tkbgo.com.tw/go_edm/edm220/index.jsp',
-        classN: '',
-        target: '_blank',
-        src: 'img/edm01-th.webp'
+        hashtag: ["UI設計"],
+        title: "放洋留遊學詳細頁設計",
+        href: "#edm06",
+        classN: "inline",
+        target: "",
+        src: "img/edm06-th.webp",
       },
       {
-        hashtag: ['UI設計', '切版'],
-        title: '中鋼行銷頁設計切版',
-        href: 'https://www.tkbgo.com.tw/go_edm/edm178/index.jsp',
-        classN: '',
-        target: '_blank',
-        src: 'img/edm05-th.webp'
+        hashtag: ["UI設計", "切版"],
+        title: "商用日文詳細頁設計切版",
+        href: "https://www.tkbgo.com.tw/go_edm/edm615/index.jsp",
+        classN: "",
+        target: "_blank",
+        src: "img/edm07-th.webp",
       },
       {
-        hashtag: ['UI設計'],
-        title: '放洋留遊學詳細頁設計',
-        href: '#edm06',
-        classN: 'inline',
-        target: '',
-        src: 'img/edm06-th.webp'
+        hashtag: ["UI設計", "切版"],
+        title: "銀行短衝詳細頁設計切版",
+        href: "https://www.tkbgo.com.tw/go_edm/edm599/index.jsp",
+        classN: "",
+        target: "_blank",
+        src: "img/edm08-th.webp",
       },
       {
-        hashtag: ['UI設計', '切版'],
-        title: '商用日文詳細頁設計切版',
-        href: 'https://www.tkbgo.com.tw/go_edm/edm615/index.jsp',
-        classN: '',
-        target: '_blank',
-        src: 'img/edm07-th.webp'
+        hashtag: ["UI設計"],
+        title: "國營聯招詳細頁設計",
+        href: "#edm09",
+        classN: "inline",
+        target: "",
+        src: "img/edm09-th.webp",
       },
       {
-        hashtag: ['UI設計', '切版'],
-        title: '銀行短衝詳細頁設計切版',
-        href: 'https://www.tkbgo.com.tw/go_edm/edm599/index.jsp',
-        classN: '',
-        target: '_blank',
-        src: 'img/edm08-th.webp'
+        hashtag: ["UI設計"],
+        title: "學習歷程檔案詳細頁設計",
+        href: "#edm04",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/edm04-th.webp",
       },
       {
-        hashtag: ['UI設計'],
-        title: '國營聯招詳細頁設計',
-        href: '#edm09',
-        classN: 'inline',
-        target: '',
-        src: 'img/edm09-th.webp'
+        hashtag: ["UI設計", "切版"],
+        title: "會員專區設計切版",
+        href: "#member01",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/member01.webp",
       },
       {
-        hashtag: ['UI設計'],
-        title: '學習歷程檔案詳細頁設計',
-        href: '#edm04',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/edm04-th.webp'
+        hashtag: ["UI設計", "切版"],
+        title: "登入註冊跳窗設計切版",
+        href: "#login01",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/login01.webp",
       },
       {
-        hashtag: ['UI設計', '切版'] ,
-        title: '會員專區設計切版',
-        href: '#member01',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/member01.webp'
-      },
-      {
-        hashtag: ['UI設計', '切版'] ,
-        title: '登入註冊跳窗設計切版',
-        href: '#login01',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/login01.webp'
-      },
-      {
-        hashtag: ['UI設計'] ,
-        title: 'ICAR登入設計',
-        href: '#login05',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/login05.webp'
+        hashtag: ["UI設計"],
+        title: "ICAR登入設計",
+        href: "#login05",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/login05.webp",
       },
     ],
-    "文宣設計": [
+    文宣設計: [
       {
-        hashtag: ['文宣設計'],
-        title: '記帳士手冊封面',
-        href: '#dm03',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/dm03.webp'
+        hashtag: ["文宣設計"],
+        title: "記帳士手冊封面",
+        href: "#dm03",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/dm03.webp",
       },
       {
-        hashtag: ['文宣設計'],
-        title: '文化行政傳單DM',
-        href: '#dm01',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/dm01-1.webp'
+        hashtag: ["文宣設計"],
+        title: "文化行政傳單DM",
+        href: "#dm01",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/dm01-1.webp",
       },
       {
-        hashtag: ['文宣設計'],
-        title: '記帳士教材封面',
-        href: '#book01',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/book01-th.webp'
-      }
+        hashtag: ["文宣設計"],
+        title: "記帳士教材封面",
+        href: "#book01",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/book01-th.webp",
+      },
     ],
-    "Banner設計": [
+    Banner設計: [
       {
-        hashtag: ['Banner設計'],
-        title: '記帳士優惠Banner',
-        href: '#banner01',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/banner01.webp'
+        hashtag: ["Banner設計"],
+        title: "記帳士優惠Banner",
+        href: "#banner01",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/banner01.webp",
       },
       {
-        hashtag: ['Banner設計'],
-        title: '銀行招考直播Banner',
-        href: '#banner02',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/banner02.webp'
+        hashtag: ["Banner設計"],
+        title: "銀行招考直播Banner",
+        href: "#banner02",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/banner02.webp",
       },
       {
-        hashtag: ['Banner設計', '插畫設計'],
-        title: '歐恩伊聯名商品Facebook廣告',
-        href: '#banner05',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/banner05.webp'
-      }
+        hashtag: ["Banner設計", "插畫設計"],
+        title: "歐恩伊聯名商品Facebook廣告",
+        href: "#banner05",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/banner05.webp",
+      },
     ],
-    "圖說設計": [
+    圖說設計: [
       {
-        hashtag: ['圖說設計'],
-        title: '高中升學路徑說明圖',
-        href: '#banner03',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/banner03.webp'
+        hashtag: ["圖說設計"],
+        title: "高中升學路徑說明圖",
+        href: "#banner03",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/banner03.webp",
       },
       {
-        hashtag: ['圖說設計'],
-        title: '前端必修課程封面圖',
-        href: '#banner04',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/banner04.webp'
-      }
+        hashtag: ["圖說設計"],
+        title: "前端必修課程封面圖",
+        href: "#banner04",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/banner04.webp",
+      },
     ],
-    "Logo設計": [
+    Logo設計: [
       {
-        hashtag: ['Logo設計'],
-        title: 'DoWell Logo',
-        href: '#logo03',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/logo03.webp'
+        hashtag: ["Logo設計"],
+        title: "DoWell Logo",
+        href: "#logo03",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/logo03.webp",
       },
       {
-        hashtag: ['Logo設計'],
-        title: 'VICVIN Logo',
-        href: 'img/logo04pdf.pdf',
-        classN: '',
-        target: '_blank',
-        src: 'img/logo04-th.webp'
-      }
+        hashtag: ["Logo設計"],
+        title: "VICVIN Logo",
+        href: "img/logo04pdf.pdf",
+        classN: "",
+        target: "_blank",
+        src: "img/logo04-th.webp",
+      },
     ],
-    "插畫設計": [
+    插畫設計: [
       {
-        hashtag: ['插畫設計'],
-        title: 'TKB日文柴犬',
-        href: '#paint01',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/paint01.webp'
+        hashtag: ["插畫設計"],
+        title: "TKB日文柴犬",
+        href: "#paint01",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/paint01.webp",
       },
       {
-        hashtag: ['插畫設計','MediBang'],
-        title: '博美犬插畫練習',
-        href: '#paint02',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/paint02.webp'
+        hashtag: ["插畫設計", "MediBang"],
+        title: "博美犬插畫練習",
+        href: "#paint02",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/paint02.webp",
       },
       {
-        hashtag: ['插畫設計'],
-        title: '米克斯插畫練習',
-        href: '#paint03',
-        classN: 'inline cboxElement',
-        target: '',
-        src: 'img/paint03.webp'
-      }
-    ]
-  }
+        hashtag: ["插畫設計"],
+        title: "米克斯插畫練習",
+        href: "#paint03",
+        classN: "inline cboxElement",
+        target: "",
+        src: "img/paint03.webp",
+      },
+    ],
+  };
 
-  var searchBar = $('.searchBar:text');
-  var sBlock = $('.blockList');
-  var sTag = $('#sBtn button');
+  var searchBar = $(".searchBar:text");
+  var sBlock = $(".blockList");
+  var sTag = $("#sBtn button");
   var sArr = [];
-  var html = '';
+  var html = "";
 
   // for迴圈不能使用匿名函示
-  for(var s = 0; s < sTag.length; s++) {
+  for (var s = 0; s < sTag.length; s++) {
+    sTag[s].addEventListener("click", function () {
+      $(this).addClass("active").siblings().removeClass("active");
 
-    sTag[s].addEventListener('click', function() {
-
-      $(this).addClass('active').siblings().removeClass('active');
-
-      if(this.id !== 'allBtn') return tagSearch(this.innerText);
+      if (this.id !== "allBtn") return tagSearch(this.innerText);
       allBlock();
-      
     });
-
   }
-
 
   // 全部顯示
   function allBlock() {
-
-    html = '';
+    html = "";
     sBlock.html(html);
 
     // 存入數組陣列
-    sArr =  Object.entries(pContent);
-    
-    var str = '';
-    for(var i = 0; i < sArr.length; i++) {
-      for(var j = 0; j < sArr[i][1].length; j++) {
+    sArr = Object.entries(pContent);
+
+    var str = "";
+    for (var i = 0; i < sArr.length; i++) {
+      for (var j = 0; j < sArr[i][1].length; j++) {
         str = sArr[i][1][j];
-        html += 
-        `
+        html += `
         <a class="${str.classN}" href="${str.href}" target="${str.target}">
           <div class="hover">
             <div class="content">
@@ -327,46 +320,39 @@ $(document).ready(function () {
           </div>
           <img class="lazyload" src="img/lazy.svg" data-src="${str.src}" alt="${str.title}">
         </a>
-        `
+        `;
       }
-  
     }
     sBlock.html(html);
-    loadMore($('.blockList a').length);
+    loadMore($(".blockList a").length);
     // console.log($('.blockList a').length);
-    
 
     $(".inline").colorbox({
-      inline: true, 
-      maxWidth: '720px',
-      width: '95%',
-      maxHeight: '95%',
+      inline: true,
+      maxWidth: "720px",
+      width: "95%",
+      maxHeight: "95%",
       scalePhotos: true,
       photo: true,
-      onOpen: function(){
-        $('body').css({ overflow: 'hidden' });
+      onOpen: function () {
+        $("body").css({ overflow: "hidden" });
       },
-      onClosed: function(){
-        $('body').css({ overflow: '' });
-      }
+      onClosed: function () {
+        $("body").css({ overflow: "" });
+      },
     });
-    
   }
 
   allBlock();
 
-  
   // 按下標籤搜尋顯示
   function tagSearch(sText) {
-
-    html = '';
+    html = "";
     sBlock.html(html);
     sArr = pContent[sText];
-    
-    
-    for(var i = 0; i < sArr.length; i++) {
-      html += 
-      `
+
+    for (var i = 0; i < sArr.length; i++) {
+      html += `
       <a class="${sArr[i].classN}" href="${sArr[i].href}" target="${sArr[i].target}">
         <div class="hover">
           <div class="content">
@@ -376,29 +362,28 @@ $(document).ready(function () {
         </div>
         <img class="lazyload" src="img/lazy.svg" data-src="${sArr[i].src}" alt="${sArr[i].title}">
       </a>
-      `
+      `;
       // data-aos="fade-right"
     }
-      // console.log('1 '+sBlock);
-      sBlock.html(html);
-      // console.log(sBlock);
+    // console.log('1 '+sBlock);
+    sBlock.html(html);
+    // console.log(sBlock);
 
-      // loadMore(sArr.length);
-      loadMore($('.blockList a').length);
+    // loadMore(sArr.length);
+    loadMore($(".blockList a").length);
 
-      $(".inline").colorbox({
-        inline: true, 
-        maxWidth: '720px',
-        width: '95%',
-        maxHeight: '95%', 
-        scalePhotos: true,
-        photo: true
-      });
-      
+    $(".inline").colorbox({
+      inline: true,
+      maxWidth: "720px",
+      width: "95%",
+      maxHeight: "95%",
+      scalePhotos: true,
+      photo: true,
+    });
   }
 
-  var keyword = '';
-  var sHtml = '';
+  var keyword = "";
+  var sHtml = "";
 
   // function DomRender() {
   //   html = '';
@@ -428,7 +413,7 @@ $(document).ready(function () {
   // $(sBtn).keyUp(function(e) {
   //   if(e.keyCode !== 13) return;
   //   if(this.value === '') return console.log('null');
-    
+
   //   if(this.value !== keyword) {
   //     resetData();
   //   }
@@ -436,41 +421,34 @@ $(document).ready(function () {
   //   keyword = this.value;
   //   function searchTag(keyword) {
   //     console.log(123);
-      
+
   //   }
   // })
 
   ////// SEARCH end
-
-  
 
   ////// LOADMORE begin
 
   // var count = $('.blockList a').length;
 
   function loadMore(count) {
-
     // 顯示前六個
-    $('.blockList a').slice(0, 12).css('display', 'block');
-    
+    $(".blockList a").slice(0, 12).css("display", "block");
+
     // 按下更多再載入四個
-    $('#moreBtn').click(function() {
-      $('.blockList a:hidden').slice(0, 4).css('display', 'block');
-      if($(".blockList a:hidden").length === 0) {
+    $("#moreBtn").click(function () {
+      $(".blockList a:hidden").slice(0, 4).css("display", "block");
+      if ($(".blockList a:hidden").length === 0) {
         $("#moreBtn").text("No More").addClass("null");
-      } 
-    })
+      }
+    });
 
     // 判斷loadmore按鈕
-    if(count <= 12) return $("#moreBtn").text("No More").addClass("null");
+    if (count <= 12) return $("#moreBtn").text("No More").addClass("null");
     $("#moreBtn").text("Load More").removeClass("null");
-
   }
 
-  
   ////// LOADMORE end
-
-
 
   ////// SWIPER begin
 
@@ -488,7 +466,7 @@ $(document).ready(function () {
   //     autoplay: {
   //       delay: 3500,
   //     },
-  
+
   //     observer: true, //修改swiper自己或子元素时，自动初始化swiper
   //     observeParents: true, //修改swiper的父元素时，自动初始化swiper
   // });
@@ -499,18 +477,18 @@ $(document).ready(function () {
   //Examples of how to assign the Colorbox event to elements
 
   $(".inline").colorbox({
-    inline: true, 
-    maxWidth: '720px',
-    width: '95%',
-    maxHeight: '95%',
+    inline: true,
+    maxWidth: "720px",
+    width: "95%",
+    maxHeight: "95%",
     scalePhotos: true,
     photo: true,
-    onOpen: function(){
-      $('body').css({ overflow: 'hidden' });
+    onOpen: function () {
+      $("body").css({ overflow: "hidden" });
     },
-    onClosed: function(){
-       $('body').css({ overflow: '' });
-    }
+    onClosed: function () {
+      $("body").css({ overflow: "" });
+    },
   });
 
   ////// COLORBOX end
@@ -521,8 +499,6 @@ $(document).ready(function () {
 
   ////// AOS end
 
-
-
   //// ex.
   // check(function() {
   //   // callback執行內容
@@ -531,6 +507,4 @@ $(document).ready(function () {
   //   // 內容1
   //   callback();
   // }
-
-
-})
+});
